38300496e8324163053b790f7902c9b37241a0da
Munk <mm.munk@gmail.com>
2012-04-08 13:48:03 +0200

    Handles setup of parser and evalution of x, y and z values

--- 3DPlotter/src/munk/graph/plot/ParametricPlotter.java
+++ 3DPlotter/src/munk/graph/plot/ParametricPlotter.java
@@ -1,76 +1,65 @@
 package munk.graph.plot;
 
-import javax.media.j3d.LineArray;
 import javax.media.j3d.Shape3D;
-import javax.vecmath.Point3f;
 
 import org.nfunk.jep.*;
 
-public class ParametricPlotter {
-	
-	private final float	tMin;
-	private final float	tMax;
-	private float stepsize;
+public abstract class ParametricPlotter {
 	
 	private JEP jep;
 	private Node xNode;
 	private Node yNode;
 	private Node zNode;
 	
-	public ParametricPlotter(String xExpr, String yExpr, String zExpr, float tMin, float tMax, float stepSize) throws ParseException {
-		this.tMin = tMin;
-		this.tMax = tMax;
-		stepsize = stepSize;
-		
+	public ParametricPlotter(String xExpr, String yExpr, String zExpr, String[] variables, float[] startValues, float stepSize) throws ParseException {
 		jep = new JEP();
 		jep.addStandardFunctions();
 		jep.addStandardConstants();
-		jep.addVariable("t", tMin);
-		jep.addVariable("x", 0);
-		jep.addVariable("y", 0);
-		jep.addVariable("z", 0);
+		
+		if (variables.length != startValues.length) {
+			throw new IllegalStateException("There must be an equal amount of startvalues and variables");
+		}
+		
+		for (int i = 0; i < variables.length; i++) {
+			String var = variables[i];
+			jep.addVariable(var, startValues[i]);
+		}
 		
 		xNode = jep.parse(xExpr);
 		yNode = jep.parse(yExpr);
 		zNode = jep.parse(zExpr);
 	}
 	
-	public ParametricPlotter(String xFunc, String yFunc, String zFunc, float tMin, float tMax) throws ParseException {
-		this(xFunc, yFunc, zFunc, tMin, tMax, 0.1f);
+	public ParametricPlotter(String xExpr, String yExpr, String zExpr, String[] variables, float[] startValues) throws ParseException {
+		this(xExpr, yExpr, zExpr, variables, startValues, 0.1f);
 	}
 	
-	public Shape3D getPlot() {
-		int length = (int) Math.ceil((tMax - tMin) / stepsize) + 1;
-		LineArray la = new LineArray(2 * (length - 1), LineArray.COORDINATES);
-		
-		float t = tMin;
-
-		
-		Point3f[] points = new Point3f[length + 1];
-		for (int i = 0; i < length; i++) {
-			jep.addVariable("t", t);
-			System.out.println(t);
-
-			try {
-				double xValue = (Double) jep.evaluate(xNode);
-				double yValue = (Double) jep.evaluate(yNode);
-				double zValue = (Double) jep.evaluate(zNode);
-				points[i] = new Point3f((float) xValue, (float) yValue, (float) zValue);
-
-			} catch (ParseException e) {
-				// Does not happen
-			}
-			t += stepsize;
-		}
-		
-		int vertice = 0;
-		
-		for (int i = 0; i < length - 1; i++) {
-			la.setCoordinate(vertice++, points[i]);
-			la.setCoordinate(vertice++, points[i+1]);
-		}
-		
-		return new Shape3D(la);
+	public void setVariable(String var, float value) {
+		jep.addVariable(var, value);
 	}
+	
+	public float getXValue() {
+		return evalNode(xNode);
+	}
+	
+	public float getYValue() {
+		return evalNode(yNode);
+	}
+	
+	public float getZValue() {
+		return evalNode(zNode);
+	}
+	
+	private float evalNode(Node node) {
+		try {
+			double value = (double) jep.evaluate(node);
+			return (float) value;
+		} catch (ParseException e) {
+			// Does not happen
+			return Float.NaN;
+		}
+	}
+	
+	public abstract Shape3D getPlot();
 
 }
