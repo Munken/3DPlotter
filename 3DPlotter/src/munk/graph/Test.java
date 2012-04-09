ce13e6e10002f8bc020b8daa76aa987c0fe4230e
Munk <mm.munk@gmail.com>
2012-04-09 14:00:49 +0200

    _

--- 3DPlotter/src/munk/graph/Test.java
+++ 3DPlotter/src/munk/graph/Test.java
@@ -1,16 +1,24 @@
 package munk.graph;
 
-import javax.vecmath.Point3f;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 
 public class Test {
 
 
 	public static void main(String[] args) {
-
+		Pattern PATTERN = Pattern.compile("([^=]+)=([^=]+)$");
+		Matcher m = PATTERN.matcher("x^2 + cos(x) + sin(y) = y");
+		boolean matches = m.matches();
+		
+		System.out.println(matches);
+		System.out.println(m.group(1) + " || " + m.group(2));
+		
+		String result = m.group(1) + "-(" + m.group(2) + ")";
+		System.out.println(result);
 	}
 	
-	private static float value(Point3f point) {
-		return point.x*point.x + point.y*point.y + point.z*point.z - 1;
-	}
+
 }
