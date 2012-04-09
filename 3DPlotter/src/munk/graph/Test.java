3c94439132f206ab853b6319aae3553c1804c719
Munk <mm.munk@gmail.com>
2012-04-09 21:07:13 +0200

    Cleanup

--- 3DPlotter/src/munk/graph/Test.java
+++ 3DPlotter/src/munk/graph/Test.java
@@ -2,6 +2,10 @@ package munk.graph;
 
 import org.nfunk.jep.*;
 
+import com.graphbuilder.math.*;
+
+
+
 import de.congrace.exp4j.*;
 
 
@@ -17,17 +21,19 @@ public class Test {
 		.withVariable("y", 0)
 		.build();
 		
-		double limit = 10E5;
+		double limit = 10E1;
+		
+		double[][] res = new double[(int) limit][2];
 		long start = System.currentTimeMillis();
-		for (double x = 0; x < limit; x++) {
-			calc.setVariable("x", x);
-			calc.setVariable("y", x);
-
-			double result1 = calc.calculate();
-		}
-		
-		System.out.println(System.currentTimeMillis() - start);
-		
+//		for (double x = 0; x < limit; x++) {
+//			calc.setVariable("x", x);
+//			calc.setVariable("y", x);
+//
+//			double result1 = calc.calculate();
+//		}
+//		
+//		System.out.println(System.currentTimeMillis() - start);
+//		
 		start = System.currentTimeMillis();
 		JEP jep = new JEP();
 		jep.addStandardConstants();
@@ -37,14 +43,46 @@ public class Test {
 		jep.addVariable("z", 0);
 		Node node = jep.parse(expr);
 		
+		int count = 0;
 		for (double x = 0; x < limit; x++) {
 			jep.setVarValue("x", x);
 			jep.setVarValue("y", x);
 			
 			double result = (double) jep.evaluate(node);
+			res[count++][0] = result;
 		}
 		
 		System.out.println(System.currentTimeMillis() - start);
+		
+		Expression ex = ExpressionTree.parse("cos(a)");
+		
+		VarMap vm = new VarMap();
+		vm.setValue("x", 0);
+		vm.setValue("y", 0);
+		
+		FuncMap fm = new FuncMap();
+		fm.loadDefaultFunctions();
+		
+		count = 0;
+		start = System.currentTimeMillis();
+		for (double x = 0; x < limit; x++) {
+			vm.setValue("x", x);
+			vm.setValue("y", x);
+			
+			double result = ex.eval(vm, fm);
+			res[count++][1] = result;
+		}
+		
+		System.out.println(System.currentTimeMillis() - start);
+		
+		count = 0;
+		for (int i = 0; i < res.length; i++) {
+//			System.out.println(res[i][0] + "  " + res[i][1]);
+			
+			if (res[i][0] != res[i][1])
+				count++;
+		}
+		System.out.println(count);
 	}
 	
 
--- 3DPlotter/src/munk/graph/plot/ImplicitPlotter.java
+++ 3DPlotter/src/munk/graph/plot/ImplicitPlotter.java
@@ -10,8 +10,6 @@ import javax.vecmath.Point3f;
 
 import munk.graph.marching.*;
 
-import org.nfunk.jep.*;
-
 import com.graphbuilder.math.*;
 import com.sun.j3d.utils.geometry.GeometryInfo;
 import com.sun.j3d.utils.geometry.NormalGenerator;
@@ -30,9 +28,6 @@ public class ImplicitPlotter {
 	
 	private float stepsize;
 	private Shape3D plot;
-	
-	private JEP jep;
-	private Node node;
 	
 	private Expression ex;
 	private VarMap vm;
@@ -61,14 +56,6 @@ public class ImplicitPlotter {
 		
 		fm = new FuncMap();
 		fm.loadDefaultFunctions();
-//		jep = new JEP();
-//		jep.addStandardFunctions();
-//		jep.addStandardConstants();
-//		jep.addVariable("x", xMin);
-//		jep.addVariable("y", yMin);
-//		jep.addVariable("z", zMin);
-//		
-//		node = jep.parse(expr);
 	}
 
 	private String preParse(String expr) {
@@ -180,6 +167,7 @@ public class ImplicitPlotter {
 			z += stepsize;
 		}
 		
+		// Build geometry from triangles
 		if (triangles.size() >= 3) {
 			GeometryInfo gi = new GeometryInfo(GeometryInfo.TRIANGLE_ARRAY);
 			Point3f[] points = new Point3f[triangles.size()];
@@ -209,21 +197,9 @@ public class ImplicitPlotter {
 	
 	private float value(Point3f point) {
 		return value(point.x, point.y, point.z);
-//		return point.x*point.x + point.y*point.y + point.z*point.z - 1;
 	}
 	
 	private float value(float x, float y, float z) {
-//		jep.addVariable("x", x);
-//		jep.addVariable("y", y);
-//		jep.addVariable("z", z);
-//		
-//		try {
-//			double value = (double) jep.evaluate(node);
-//			return (float) value;
-//		} catch (ParseException e) {
-//			return Float.NaN;
-//		}
-		
 		vm.setValue("x", x);
 		vm.setValue("y", y);
 		vm.setValue("z", z);
@@ -262,117 +238,8 @@ public class ImplicitPlotter {
 			upperValues[j][1] = value(x, y, z);
 			y += stepsize;
 		}
-
-		
-		/* Overkill 
-		float y = yMin;
-		for (int j = 0; j <= 1; j++) {
-			
-			float x = xMin;
-			for (int i = 0; i < xLength; i++) {
-			
-				upperValues[j][i] = value(x, y, z);
-				x += stepsize;
-			}
-			
-			y += stepsize;
-		}
-		
-		for (int j = 0; j < yLength; j++) {
-			
-			float x = xMin;
-			for (int i = 0; i <= 1; i++) {
-			
-				upperValues[j][i] = value(x, y, z);
-				x += stepsize;
-			}
-			
-			y += stepsize;
-		}
-		*/
 	}
-	
-//	private Shape3D plot() {
-//		MarchingCubes m = new MarchingCubes();
-//
-//		Point3f[] corners = new Point3f[8];
-//		float[] values = new float[8];
-//		Triangle[] tri = new Triangle[5];
-//
-//		for (int q = 0; q < tri.length; q++) {
-//			tri[q] = new Triangle();
-//		}
-//		for (int q = 0; q < corners.length; q++) {
-//			corners[q] = new Point3f();
-//		}
-//		
-//		List<Triangle> triangles = new ArrayList<Triangle>();
-//		for (float z = zMin; z <= zMax; z += stepsize) {
-//			for (float y = yMin; y <= yMax; y += stepsize) {
-//				for (float x = xMin; x < xMax; x += stepsize) {
-//					corners[0].x = x;
-//					corners[0].y = y;
-//					corners[0].z = z;
-//					values[0] = value(corners[0]);
-//					
-//					corners[1].x = x+stepsize;
-//					corners[1].y = y;
-//					corners[1].z = z; 
-//					values[1] = value(corners[1]);
-//					
-//					corners[2].x = x+ stepsize;
-//					corners[2].y = y+ stepsize;
-//					corners[2].z = z;
-//					values[2] = value(corners[2]);
-//					
-//					corners[3].x = x;
-//					corners[3].y = y+ stepsize;
-//					corners[3].z = z;
-//					values[3] = value(corners[3]);
-//					
-//					corners[4].x = x;
-//					corners[4].y = y;
-//					corners[4].z = z+ stepsize;
-//					values[4] = value(corners[4]);
-//					
-//					corners[5].x = x+ stepsize;
-//					corners[5].y = y;
-//					corners[5].z = z+ stepsize;
-//					values[5] = value(corners[5]);
-//					
-//					corners[6].x = x+ stepsize;
-//					corners[6].y = y+ stepsize;
-//					corners[6].z = z+ stepsize;
-//					values[6] = value(corners[6]);
-//					
-//					corners[7].x = x;
-//					corners[7].y = y+ stepsize;
-//					corners[7].z = z+ stepsize;
-//					values[7] = value(corners[7]);
-//					
-//					GridCell grid = new GridCell(corners, values);
-//					
-//					int facets = m.Pologynise(grid, tri, 0);
-//					
-//					if (facets > 0) {
-//						for (int i = 0; i < facets; i++) {
-//							Triangle newTri = new Triangle(tri[i]);
-//							triangles.add(newTri);
-//						}
-//					}
-//				}
-//			}
-//		}
-//		TriangleArray triArray = new TriangleArray(3*triangles.size(), TriangleArray.COORDINATES);
-//		int vertice = 0;
-//		for (Triangle t : triangles) {
-//			for (int i = 0; i < 3; i++) {
-//				triArray.setCoordinate(vertice++, t.getVertex(i));
-//			}
-//		}
-//		
-//		
-//		return new Shape3D(triArray);
-//	}
+		
+
 
 }
