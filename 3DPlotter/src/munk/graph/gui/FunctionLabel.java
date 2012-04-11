ce609ff793fc661d3cf9afc27d4a5a2426094892
EMHER <emil.h.eriksen@gmail.com>
2012-04-11 22:44:52 +0200

    Delete button added along width new button icons.

--- 3DPlotter/src/munk/graph/gui/FunctionLabel.java
+++ 3DPlotter/src/munk/graph/gui/FunctionLabel.java
@@ -9,6 +9,8 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JCheckBox;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -23,6 +25,7 @@ public class FunctionLabel extends JPanel{
 	JTextField exprField;
 	Function mother;
 	ActionListener listener;
+	private JButton btnDelete;
 
 	public FunctionLabel (Function function, ActionListener a){
 		this.mother = function;
@@ -30,18 +33,18 @@ public class FunctionLabel extends JPanel{
 		
 		// GUI representation
 		GridBagLayout gbl_fLabel = new GridBagLayout();
-		gbl_fLabel.columnWidths = new int[]{30, 170, 15, 0};
+		gbl_fLabel.columnWidths = new int[]{15, 150, 0, 15, 0};
 		gbl_fLabel.rowHeights = new int[]{0, 0};
-		gbl_fLabel.columnWeights = new double[]{0.0, 1.0, 0.0, Double.MIN_VALUE};
+		gbl_fLabel.columnWeights = new double[]{0.0, 1.0, 0.0, 0.0, Double.MIN_VALUE};
 		gbl_fLabel.rowWeights = new double[]{0.0, Double.MIN_VALUE};
 		this.setLayout(gbl_fLabel);
 		
-		JButton btnEdit = new JButton("Edit");
-		GridBagConstraints gbc_btnEdit = new GridBagConstraints();
-		gbc_btnEdit.insets = new Insets(0, 0, 0, 5);
-		gbc_btnEdit.gridx = 0;
-		gbc_btnEdit.gridy = 0;
-		add(btnEdit, gbc_btnEdit);
+		btnDelete = new JButton(new ImageIcon("delete.png"));
+		GridBagConstraints gbc_btnDelete = new GridBagConstraints();
+		gbc_btnDelete.insets = new Insets(0, 0, 0, 5);
+		gbc_btnDelete.gridx = 0;
+		gbc_btnDelete.gridy = 0;
+		add(btnDelete, gbc_btnDelete);
 
 		exprField = new JTextField(mother.getExpression()[0]);
 		GridBagConstraints gbc_list = new GridBagConstraints();
@@ -52,12 +55,21 @@ public class FunctionLabel extends JPanel{
 		this.add(exprField, gbc_list);
 		exprField.setEditable(true);
 		
-		chckbx = new JCheckBox("");
-		GridBagConstraints gbc_chckbxTest = new GridBagConstraints();
-		gbc_chckbxTest.gridx = 2;
-		gbc_chckbxTest.gridy = 0;
-		chckbx.setSelected(true);
-		this.add(chckbx, gbc_chckbxTest);
+		JButton btnEdit = new JButton(new ImageIcon("edit.png"));
+		GridBagConstraints gbc_btnEdit = new GridBagConstraints();
+		gbc_btnEdit.insets = new Insets(0, 0, 0, 5);
+		gbc_btnEdit.gridx = 2;
+		gbc_btnEdit.gridy = 0;
+		add(btnEdit, gbc_btnEdit);
+		
+		// Delete function on click.
+		btnDelete.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				listener.actionPerformed(new ActionEvent(mother, 2, exprField.getText()));
+			}
+		});
 		
 		// Spawn edit dialog on click.
 		btnEdit.addActionListener(new ActionListener() {
@@ -67,6 +79,13 @@ public class FunctionLabel extends JPanel{
 				listener.actionPerformed(new ActionEvent(mother, 1, exprField.getText()));
 			}
 		});
+		
+		chckbx = new JCheckBox("");
+		GridBagConstraints gbc_chckbxTest = new GridBagConstraints();
+		gbc_chckbxTest.gridx = 3;
+		gbc_chckbxTest.gridy = 0;
+		chckbx.setSelected(true);
+		this.add(chckbx, gbc_chckbxTest);
 
 		//If text is updated, color the cell RED. When "enter" is pressed, process the new function, and recolor the cell WHITE.
 		exprField.addKeyListener(new KeyAdapter() {
