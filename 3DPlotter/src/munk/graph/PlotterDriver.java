f5fa53fdcd2bd180d054128652df4bf254632d58
Munk <mm.munk@gmail.com>
2012-04-12 12:20:00 +0200

    Iterative implicit is functional.
    There is a bug. It has dead pixel

--- 3DPlotter/src/munk/graph/PlotterDriver.java
+++ 3DPlotter/src/munk/graph/PlotterDriver.java
@@ -19,7 +19,7 @@ public class PlotterDriver {
 		final JFrame frame = new JFrame("Mega ultra");
 		
 		final Plotter3D p = new Plotter3D();
-		final float i = 1.2f;
+		final float i = 2.2f;
 		
 //		float[] bounds = {0, (float) (2*Math.PI)};
 //		ParametricFunction pp = new ParametricFunction("0.5*cos(u)", "0.5*sin(u)", "u/10", Colors.RED, bounds, 0.1f);
@@ -39,8 +39,12 @@ public class PlotterDriver {
 //		ImplicitFunction ip = new ImplicitFunction(expr, Colors.RED, recBound, 0.05f);
 //		p.plotFunction(ip);
 		
-		ImplicitIterativeFunction rec = new ImplicitIterativeFunction(expr, Colors.BLUE, recBound, 0.1f);
+		ImplicitRecursiveFunction rec = new ImplicitRecursiveFunction(expr, Colors.BLUE, recBound, 0.05f);
 		p.plotFunction(rec);
+		
+//		ImplicitIterativeFunction it = new ImplicitIterativeFunction(expr, Colors.BLUE, recBound, 0.05f);
+//		p.plotFunction(it);
+		
 //		p.plotFunction("z = y", -i, i, -i, i, Colors.MAGENTA);
 //        p.plotFunction(-i, i, -i, i, "z = x", Colors.RED);
 //        p.plotFunction(-i, i, -i, i, "x = y", Colors.CYAN);
